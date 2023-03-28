4a9ee49b78a8a86360989932ad54ab586a5e89fe
anjilakarkigit <anjilagithub@gmail.com>
2023-03-28 09:10:55 -0500

    Added JWT middleware and [Authorize] filter

--- Services/Interviews/Interviews.API/Program.cs
+++ Services/Interviews/Interviews.API/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,21 +11,35 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var dockerConnectionString = Environment.GetEnvironmentVariable("MSSQLConnectionString");
+// Microsoft.AspNetCore.Authentication.JwtBearer
+// Microsoft.IdentityModel.Tokens
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme  )
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "HRM",
+            ValidAudience = "HRM Users",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
+        };
+    } );
+//This is going to populate httpcontext with claims,, roles,etc
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
